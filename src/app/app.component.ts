8ef2720c33860b34394ed262e44d2c379c376f3d
Marcello Silverio <smarcelloc@gmail.com>
2020-12-10 04:43:46 +0000

    Adicionando lista no projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app-todo-com-angular';
+  public todos: any[] = []; // [ ]
+  //public todos:any[]; // undefined
+  public title: String = "Minhas tarefas"
+
+  constructor() {
+    this.todos.push('Passear com o cachorro')
+    this.todos.push('ir a praia')
+    this.todos.push('visitar a tia no hospital')
+  }
+
+  public alterarTitle(): void {
+    this.title = "titulo alterado"
+  }
 }
